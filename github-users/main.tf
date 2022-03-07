8794874c7270e6ade46198c1413ad4d9db5b5e61
mallikarjun1137 <mallikarjun1137@gmail.com>
2022-03-07 23:45:21 +0530

    its my first commit

--- github-users/main.tf
+++ github-users/main.tf
@@ -1,2 +1,9 @@
-provide "git"
+provider "github" { 
+  organization = "myorganisation1137"
+  token = "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"
+}
 
+resource "github_membership" "user" {
+  username = "mallikarjun1137"
+  role = "member"
+}
